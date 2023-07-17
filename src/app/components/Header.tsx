711f3fb2d361ad090ccc5c60d9a915da7bcd046b
Mark <mphill05@gmail.com>
2023-07-17 00:38:22 -0500

    functioning navbar with minimal styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,64 @@
-export default function Header() {
-  return <div>Header</div>;
-}
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import styles from './Header.module.scss';
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>Logo</div>
+      <div
+        className={`${styles.hamburger} ${isMenuOpen ? styles.open : ''}`}
+        onClick={toggleMenu}
+      >
+        <span className={isMenuOpen ? styles.white : ''}></span>
+        <span className={isMenuOpen ? styles.white : ''}></span>
+        <span className={isMenuOpen ? styles.white : ''}></span>
+      </div>
+      <div
+        className={`${styles.menuOverlay} ${isMenuOpen ? styles.open : ''}`}
+        onClick={toggleMenu}
+      />
+      <ul className={`${styles.menu} ${isMenuOpen ? styles.open : ''}`}>
+        <li>
+          <Link href="/" onClick={closeMenu}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/music" onClick={closeMenu}>
+            Music
+          </Link>
+        </li>
+        <li>
+          <Link href="/store" onClick={closeMenu}>
+            Store
+          </Link>
+        </li>
+        <li>
+          <Link href="/tour" onClick={closeMenu}>
+            Tour
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" onClick={closeMenu}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
