3e09486ba33e233a6b04cf707e929c4332eda486
Mark <mphill05@gmail.com>
2023-08-29 17:10:42 -0500

    added functioning searchbar

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { Card } from '@/components/Card/Card';
 import { songs } from '../../data/songs';
 import styles from './page.module.scss';
+import { useState } from 'react';
+import { SearchBar } from '@/components/searchBar/searchBar';
 
 interface Song {
   imageUrl: string;
@@ -8,17 +12,26 @@ interface Song {
 }
 
 export default function Music() {
+  const [query, setQuery] = useState('');
+
+  const filteredSongs = songs.filter((song) =>
+    song.title.toLowerCase().includes(query.toLocaleLowerCase())
+  );
+
   return (
-    <div className={styles.cardContainer}>
-      {songs.map((song: Song, index: number) => (
-        <Card
-          key={index}
-          imageUrl={song.imageUrl}
-          title={song.title}
-          height={250}
-          width={250}
-        />
-      ))}
+    <div>
+      <SearchBar query={query} setQuery={setQuery} />
+      <div className={styles.cardContainer}>
+        {filteredSongs.map((song: Song, index: number) => (
+          <Card
+            key={index}
+            imageUrl={song.imageUrl}
+            title={song.title}
+            height={250}
+            width={250}
+          />
+        ))}
+      </div>
     </div>
   );
 }
