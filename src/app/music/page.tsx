d385c7117630bb59b3c7e8d7792bd25601038589
Mark <mphill05@gmail.com>
2023-09-04 23:54:00 -0500

    basic layout for music and store pages with product page in the basic shape

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,11 +1,37 @@
 'use client';
 
-import { Card } from '@/components/Card/Card';
 import { songs } from '../../data/songs';
 import styles from './page.module.scss';
 import { useState } from 'react';
 import { SearchBar } from '@/components/searchBar/searchBar';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+let easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 interface Song {
   id: string;
@@ -23,19 +49,47 @@ export default function Music() {
   return (
     <div>
       <SearchBar query={query} setQuery={setQuery} />
-      <div className={styles.cardContainer}>
-        {filteredSongs.map((song: Song, index: number) => (
-          <Link href={`/music/${song.id}`} key={song.id}>
-            <Card
-              key={index}
-              imageUrl={song.imageUrl}
-              title={song.title}
-              height={250}
-              width={250}
-            />
-          </Link>
-        ))}
-      </div>
+      <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
+        <div className={styles.musicContainer}>
+          <div className={styles.musicCenter}>
+            <motion.div
+              animate={{ opacity: 1 }}
+              initial={{ opacity: 0 }}
+              className={styles.title}
+            >
+              <div className={styles.musicCardContainer}>
+                {filteredSongs.map((song: Song, index: number) => (
+                  <Link
+                    href={`/music/${song.id}`}
+                    key={song.id}
+                    as={`/music/${song.id}`}
+                    style={{ textDecoration: 'none' }}
+                  >
+                    <motion.div
+                      variants={fadeInUp}
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                      className={`${styles.card} no-underline`}
+                    >
+                      <motion.img
+                        initial={{ x: 60, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ delay: 0.2 }}
+                        key={song.id}
+                        src={song.imageUrl}
+                        width={250}
+                      />
+                      <div className={styles.musicInfo}>
+                        <h4>{song.title}</h4>
+                      </div>
+                    </motion.div>
+                  </Link>
+                ))}
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </motion.div>
     </div>
   );
 }
--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -6,6 +6,33 @@ import { storeItems } from '../../data/storeItems';
 import styles from './page.module.scss';
 import { SearchBar } from '@/components/searchBar/searchBar';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+let easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export default function Store() {
   const [query, setQuery] = useState('');
@@ -17,20 +44,48 @@ export default function Store() {
   return (
     <div>
       <SearchBar query={query} setQuery={setQuery} />
-      <div className={styles.cardContainer}>
-        {filteredStoreItems.map((item, index) => (
-          <Link href={`/store/${item.id}`} key={item.id}>
-            <Card
-              key={index}
-              imageUrl={item.imageUrl}
-              title={item.title}
-              price={item.price}
-              width={500}
-              height={500}
-            />
-          </Link>
-        ))}
-      </div>
+      <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
+        <div className={styles.productContainer}>
+          <div className={styles.productCenter}>
+            <motion.div
+              animate={{ opacity: 1 }}
+              initial={{ opacity: 0 }}
+              className={styles.title}
+            >
+              <div className={styles.productCardContainer}>
+                {filteredStoreItems.map((item, index) => (
+                  <Link
+                    href={`/store/${item.id}`}
+                    key={index}
+                    as={`/store/${item.id}`}
+                    style={{ textDecoration: 'none' }}
+                  >
+                    <motion.div
+                      variants={fadeInUp}
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                      className={styles.card}
+                    >
+                      <motion.img
+                        initial={{ x: 60, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ delay: 0.2 }}
+                        key={item.id}
+                        src={item.imageUrl}
+                        width={250}
+                      />
+                      <div className={styles.productInfo}>
+                        <h4>{item.title}</h4>
+                        <span>${item.price}</span>
+                      </div>
+                    </motion.div>
+                  </Link>
+                ))}
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </motion.div>
     </div>
   );
 }
