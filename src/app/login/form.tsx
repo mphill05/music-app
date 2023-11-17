cb80304cf27b19a8b3bae7576b790351a2cd34fb
Mark <mphill05@gmail.com>
2023-11-17 02:38:43 -0600

    updates to the login forms

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -2,10 +2,19 @@
 
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import styles from './form.module.scss';
+import Link from 'next/link';
+import { FaEnvelope, FaFacebook, FaGoogle, FaLock } from 'react-icons/fa';
 
 export default function Form() {
   const router = useRouter();
+  const [passwordType, setPasswordType] = useState('password');
+
+  const togglePasswordVisibility = () => {
+    setPasswordType(passwordType === 'password' ? 'text' : 'password');
+  };
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
@@ -24,9 +33,59 @@ export default function Form() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name="email" type="email" />
-      <input name="password" type="password" />
-      <button type="submit">Login</button>
+      <div className={styles.content}>
+        <header>Login</header>
+        <div className={styles.field}>
+          <span className="fa fa-user">
+            <FaEnvelope />
+          </span>
+          <input name="email" type="email" placeholder=" " required />
+          <label htmlFor="email">Email</label>
+        </div>
+        <div className={`${styles.field} ${styles.space}`}>
+          <span className="fa fa-lock">
+            <FaLock />
+          </span>
+          <input
+            name="password"
+            type="password"
+            className={styles.passKey}
+            placeholder=" "
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <span className={styles.show} onClick={togglePasswordVisibility}>
+            {passwordType === 'password' ? 'SHOW' : 'HIDE'}
+          </span>
+        </div>
+        <div className={styles.pass}>
+          <a href="#">Forgot Password?</a>
+        </div>
+        <div className={styles.field}>
+          <button className={styles.loginBtn} type="submit" value="LOGIN">
+            LOGIN
+          </button>
+        </div>
+        <div className={styles.login}>Or login with</div>
+        <div className={styles.links}>
+          <div className={styles.google}>
+            <i className="fab fa-google">
+              <FaGoogle />
+              <span>Gmail</span>
+            </i>
+          </div>
+          <div className={styles.facebook}>
+            <i className="fab fa-facebook-f">
+              <FaFacebook />
+              <span>Facebook</span>
+            </i>
+          </div>
+        </div>
+        <div className={styles.signup}>
+          {"Don't have account? "}
+          <Link href={'/register'}>Signup</Link>
+        </div>
+      </div>
     </form>
   );
 }
